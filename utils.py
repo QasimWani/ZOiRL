f0eaf20add264b6944017520fbf3cf7c525754d0
Qasim Wani <qasim31wani@gmail.com>
2021-06-22 03:21:45 -0700

    adding buffer class

--- utils.py
+++ utils.py
@@ -1,291 +1,123 @@
-from critic import Critic
-from actor import Actor
-
-from citylearn import CityLearn
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-### Actor + Critic Utils
+from collections import deque, namedtuple
+import torch
+import random
 
 
-def Q1(self, state, action) -> Critic:
-    ### input data for local critic
-    return Critic(data_target_1).forward()  ### G_t_lambda_target_1, E_grid_target_1
+# Set to cuda (gpu) instance if compute available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# source: https://gist.github.com/enochkan/56af870bd19884f189639a0cb3381ff4#file-adam_optim-py
+# update rule is as simple as... (https://towardsdatascience.com/how-to-implement-an-adam-optimizer-from-scratch-76e7b217f1cc)
+# > w_0 = adam.update(t,w=w_0, b=b_0, dw=dw, db=db)
+class Adam:
+    def __init__(self, eta=0.01, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        self.m_dw, self.v_dw = 0, 0
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.eta = eta
+
+    def update(self, t, w, dw):
+        ## dw, db are from current minibatch
+        ## momentum beta 1
+        # *** weights *** #
+        self.m_dw = self.beta1 * self.m_dw + (1 - self.beta1) * dw
+
+        ## rms beta 2
+        # *** weights *** #
+        self.v_dw = self.beta2 * self.v_dw + (1 - self.beta2) * (dw ** 2)
+
+        ## bias correction
+        m_dw_corr = self.m_dw / (1 - self.beta1 ** t)
+        v_dw_corr = self.v_dw / (1 - self.beta2 ** t)
+
+        ## update weights and biases
+        w = w - self.eta * (m_dw_corr / (np.sqrt(v_dw_corr) + self.epsilon))
+        return w
 
 
-def Q2(self, state, action) -> Actor:
-    ### input data for target critic
-    return Critic(data_target_2).forward()  ### G_t_lambda_target_2, E_grid_target_2
+BUFFER_SIZE = 24 * 2  # max number of experiences in a buffer - length of meta-episode
+MINI_BATCH = 100  # number of samples to collect from buffer (current day)
 
 
-def obtain_target_Q(Q1, Q2) -> tuple:
-    Q1, E_grid1 = Q1
-    Q2, E_grid2 = Q2
-    E_grid = [E_grid1, E_grid2]
-    return min(Q1, Q2), E_grid[np.argmin(Q1, Q2)]  ### y_t, min(Q1, Q2) of E_grid
-
-
-### CityLearn utils
-
-
-def weather_data(env: CityLearn) -> np.array:
-    ### load weather data for calculation of COP
-
-    with open(env.data_path / env.weather_file) as csv_file:
-        weather_data = pd.read_csv(csv_file)
-    weather_data = weather_data["Outdoor Drybulb Temperature [C]"]
-    return weather_data
-
-
-def parse_data(data: dict, current_data: dict) -> list:
-    """Parses `current_data` for optimization and loads into `data`"""
-    assert (
-        len(current_data) == 28
-    ), "Invalid number of parameters. Can't run basic (root) agent optimization"
-
-    for key, value in current_data.items():
-        if key not in data:
-            data[key] = []
-        data[key].append(value)
-
-    # for key, value in current_data.items():
-    #         if np.array(data[key]).shape == (1, 9):  # removes duplicates
-    #                 data[key] = [value]
-    return data
-
-
-def get_dimensions(data: dict):
-    """Gets shape of each param"""
-    for key in data.keys():
-        print(data[key].shape)
-
-
-def get_building(data: dict, building_id: int):
-    """Loads data (dict) from a particular building. 1-based indexing for building"""
-    assert building_id > 0, "building_id is 1-based indexing."
-    building_data = {}
-    for key in data.keys():
-        building_data[key] = np.array(data[key])[:, building_id - 1]
-    return building_data
-
-
-def convert_to_numpy(params: dict):
-    """Converts dic[key] to nd.array"""
-    for key in params:
-        if key == "c_bat_init" or key == "c_Csto_init" or key == "c_Hsto_init":
-            params[key] = np.array(params[key][0])
-        else:
-            params[key] = np.array(params[key])
-
-
-def create_random_data(data: dict):
-    """Creates random Gaussian data"""
-    for key in data:
-        data[key] = np.clip(np.random.random(size=data[key].shape), 0, 1)
-    return data
-
-
-def get_actions(
-    data: dict,
-    Optim: Actor,
-    t,
-    debug=False,
-    apply_seed=False,
-    lookahead=False,
-    num_buildings=9,
-) -> list:
+class ReplayBuffer:
     """
-    Runs `Optim` for all 9 buildings per hour.
-    `lookahead` once set to `True` computes day-ahead dispatch
+    Implementation of a fixed size replay buffer as used in DQN algorithms.
+    The goal of a replay buffer is to unserialize relationships between sequential experiences, gaining a better temporal understanding.
     """
-    convert_to_numpy(data)  # modify in-place
-    data = create_random_data(deepcopy(data)) if apply_seed else data
-    if debug:
-        return [
-            Optim(t, data, i, actions_spaces[i].shape[0]) for i in range(num_buildings)
-        ]
 
-    return [
-        Optim(t, data, i, actions_spaces[i].shape[0]).solve(debug, lookahead)
-        for i in range(9)
-    ]
+    def __init__(self, buffer_size=BUFFER_SIZE, batch_size=MINI_BATCH):
+        """
+        Initializes the buffer.
+        @Param:
+        1. action_size: env.action_space.shape[0]
+        2. buffer_size: Maximum length of the buffer for extrapolating all experiences into trajectories. default - 1e6 (Source: DeepMind)
+        3. batch_size: size of mini-batch to train on. default = 64.
+        """
+        self.replay_memory = deque(
+            maxlen=buffer_size
+        )  # Experience replay memory object
+        self.batch_size = batch_size
+        self.experience = namedtuple(
+            "Experience",
+            field_names=["state", "action", "reward", "next_state", "done"],
+        )  # standard S,A,R,S',done
 
+    def add(self, state, action, reward, next_state, done):
+        """Adds an experience to existing memory"""
+        trajectory = self.experience(state, action, reward, next_state, done)
+        self.replay_memory.append(trajectory)
 
-def get_current_data_oracle(env: CityLearn, t: int):
-    """Returns data:dic for each building from `env` for `t` timestep"""
-    ### FB - Full batch. Trim output X[:time-step]
-    ### CT - current timestep only. X = full_data[time-step], no access to full_data
-    ### DP - dynamic update. time-step k = [... k], time-step k+n = [... k + n].
-    ### P - constant value across all time steps. changes per building only.
-
-    _max_load = 2 * 168  # 2-week max load
-    _num_buildings = len(actions_spaces)  # total number of buildings in env.
-    _start = max(t - _max_load, 0)
-    observation_data = {}
-
-    p_ele = [
-        2 if 10 <= t % 24 <= 20 else 0.2 for i in range(1, _num_buildings + 1)
-    ]  # FB -- virtual electricity price.
-    # can't get future data since action dependent
-    E_grid_past = [
-        0 for i in range(1, _num_buildings + 1)
-    ]  # FB -- replace w/ per building cost
-    ramping_cost_coeff = [
-        0.1 for i in range(_num_buildings)
-    ]  # P  -- initialized to 0.1, learned through diff.
-
-    # Loads
-    E_ns = [
-        env.buildings["Building_" + str(i)].sim_results["non_shiftable_load"][t]
-        for i in range(1, _num_buildings + 1)
-    ]  # CT
-    H_bd = [
-        env.buildings["Building_" + str(i)].sim_results["dhw_demand"][t]
-        for i in range(1, _num_buildings + 1)
-    ]  # DP
-    C_bd = [
-        env.buildings["Building_" + str(i)].sim_results["cooling_demand"][t]
-        for i in range(1, _num_buildings + 1)
-    ]  # DP
-    H_max = np.max(
-        [
-            env.buildings["Building_" + str(i)].sim_results["dhw_demand"]
-            for i in range(1, _num_buildings + 1)
-        ],
-        axis=1,
-    )  # DP
-    C_max = np.max(
-        [
-            env.buildings["Building_" + str(i)].sim_results["cooling_demand"]
-            for i in range(1, _num_buildings + 1)
-        ],
-        axis=1,
-    )  # DP
-    E_max = np.max(
-        [
-            env.buildings["Building_" + str(i)].sim_results["non_shiftable_load"]
-            for i in range(1, _num_buildings + 1)
-        ],
-        axis=1,
-    )  # DP
-
-    # PV generations
-    E_pv = [
-        env.buildings["Building_" + str(i)].sim_results["solar_gen"][t]
-        for i in range(1, _num_buildings + 1)
-    ]  # CT
-
-    # Heat Pump
-    eta_hp = [0.22] * _num_buildings  # P
-    t_C_hp = [8] * _num_buildings  # P target cooling temperature (universal constant)
-
-    COP_C = [None for i in range(_num_buildings)]  # DP
-
-    E_hpC_max = [None] * _num_buildings
-    for i in range(1, _num_buildings + 1):
-        COP_C_t = (
-            eta_hp[i - 1]
-            * float(t_C_hp[i - 1] + 273.15)
-            / (weather_data - t_C_hp[i - 1])
+    def sample(self, is_random=False):
+        """Picks all samples within the replay_buffer"""
+        experiences = (
+            random.sample(self.replay_memory, k=self.batch_size)
+            if is_random
+            else self.replay_memory[-self.batch_size :]
         )
-        COP_C_t[COP_C_t < 0] = 20.0
-        COP_C_t[COP_C_t > 20] = 20.0
-        COP_C_t = COP_C_t.to_numpy()
-        COP_C[i - 1] = COP_C_t[t]
-        E_hpC_max[i - 1] = np.max(
-            env.buildings["Building_" + str(i)].sim_results["cooling_demand"] / COP_C_t
+
+        states = (
+            torch.from_numpy(np.vstack([e.state for e in experiences if e is not None]))
+            .float()
+            .to(device)
         )
-        # max_soc = np.max(env.buildings['Building_'+str(i)].cooling_storage.soc[:t])
-        # E_hpC_max[i - 1] = max(E_hpC_max[i - 1], max_soc)
+        actions = (
+            torch.from_numpy(
+                np.vstack([e.action for e in experiences if e is not None])
+            )
+            .float()
+            .to(device)
+        )
+        rewards = (
+            torch.from_numpy(
+                np.vstack([e.reward for e in experiences if e is not None])
+            )
+            .float()
+            .to(device)
+        )
+        next_states = (
+            torch.from_numpy(
+                np.vstack([e.next_state for e in experiences if e is not None])
+            )
+            .float()
+            .to(device)
+        )
+        dones = (
+            torch.from_numpy(
+                np.vstack([e.done for e in experiences if e is not None]).astype(
+                    np.uint8
+                )
+            )
+            .float()
+            .to(device)
+        )
 
-        # except ValueError:
-        #     pass
+        return states, actions, rewards, next_states, dones
 
-    # Electric Heater
-    eta_ehH = [0.9] * _num_buildings  # P
-    # replaced capacity (not avaiable in electric heater) w/ nominal_power
-    E_ehH_max = [H_max[i] / eta_ehH[i] for i in range(_num_buildings)]  # P
-
-    # Battery
-    C_f_bat = [0.0000 for i in range(_num_buildings)]  # P
-    C_p_bat = [60] * _num_buildings  # P (range: [20, 200])
-    eta_bat = [1] * _num_buildings  # P
-    # current hour soc. normalized
-    c_bat_init = [None] * _num_buildings  # can't get future data since action dependent
-    for i in range(1, _num_buildings + 1):
-        building = env.buildings["Building_" + str(i)].electrical_storage
-        try:
-            c_bat_init[i - 1] = building.soc[t - 1] / building.capacity
-        except:
-            c_bat_init[i - 1] = 0
-    # Heat (Energy->dhw) Storage
-    C_f_Hsto = [0.00] * _num_buildings  # P
-    C_p_Hsto = [3 * H_max[i] for i in range(_num_buildings)]  # P
-    eta_Hsto = [1] * _num_buildings  # P
-    # current hour soc. normalized
-    c_Hsto_init = [
-        None
-    ] * _num_buildings  # can't get future data since action dependent
-    for i in range(1, _num_buildings + 1):
-        building = env.buildings["Building_" + str(i)].dhw_storage
-        try:
-            c_Hsto_init[i - 1] = building.soc[t - 1] / building.capacity
-        except:
-            c_Hsto_init[i - 1] = 0
-        # Cooling (Energy->cooling) Storage
-    C_f_Csto = [0.00] * _num_buildings  # P
-    C_p_Csto = [2 * C_max[i] for i in range(_num_buildings)]  # P
-    eta_Csto = [1] * _num_buildings  # P
-    # current hour soc. normalized
-    c_Csto_init = [
-        None
-    ] * _num_buildings  # can't get future data since action dependent
-    for i in range(1, _num_buildings + 1):
-        building = env.buildings["Building_" + str(i)].cooling_storage
-        try:
-            c_Csto_init[i - 1] = building.soc[t - 1] / building.capacity
-        except:
-            c_Csto_init[i - 1] = 0
-
-    ### TODO add alphas for critic params
-
-    # fill data
-    observation_data["p_ele"] = p_ele
-    observation_data["ramping_cost_coeff"] = ramping_cost_coeff
-    observation_data["E_grid_past"] = E_grid_past
-
-    observation_data["E_ns"] = E_ns
-    observation_data["H_bd"] = H_bd
-    observation_data["C_bd"] = C_bd
-    observation_data["H_max"] = H_max
-    observation_data["C_max"] = C_max
-
-    observation_data["E_pv"] = E_pv
-
-    observation_data["E_hpC_max"] = E_hpC_max
-    observation_data["eta_hp"] = eta_hp
-    observation_data["t_C_hp"] = t_C_hp
-    observation_data["COP_C"] = COP_C
-
-    observation_data["eta_ehH"] = eta_ehH
-    observation_data["E_ehH_max"] = E_ehH_max
-
-    observation_data["C_f_bat"] = C_f_bat
-    observation_data["C_p_bat"] = C_p_bat
-    observation_data["eta_bat"] = eta_bat
-    observation_data["c_bat_init"] = c_bat_init
-    observation_data["c_bat_end"] = [0.1] * _num_buildings
-
-    observation_data["C_f_Hsto"] = C_f_Hsto
-    observation_data["C_p_Hsto"] = C_p_Hsto
-    observation_data["eta_Hsto"] = eta_Hsto
-    observation_data["c_Hsto_init"] = c_Hsto_init
-
-    observation_data["C_f_Csto"] = C_f_Csto
-    observation_data["C_p_Csto"] = C_p_Csto
-    observation_data["eta_Csto"] = eta_Csto
-    observation_data["c_Csto_init"] = c_Csto_init
-
-    return observation_data
+    def __len__(self):  # override default __len__ operator
+        """Return the current size of internal memory."""
+        return len(self.replay_memory)
