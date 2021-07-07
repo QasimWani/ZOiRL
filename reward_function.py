2c35a975ee813f8a768feb870577f9796d035ef9
vanshajkhattar <50736292+vanshajkhattar@users.noreply.github.com>
2021-07-07 12:19:47 -0400

    Add files via upload

--- reward_function.py
+++ reward_function.py
@@ -3,7 +3,78 @@ This function is intended to wrap the rewards returned by the CityLearn RL envir
 be modified at will. This reward_function takes all the electrical demands and carbon intensity of all the buildings and turns them into one or multiple rewards for the agent(s)
 """
 import numpy as np
-from utils import ReplayBuffer
+
+class DemandBuffer:
+    """
+    Implementation of a fixed size replay buffer.
+    """
+
+    def __init__(self, buffer_size:int = 7, electricity_demand):
+        """
+        Initializes the buffer.
+        """
+        self.electricity_demand = electricity_demand
+        self.replay_memory = deque([electric_demand])  # Experience replay memory object
+        
+        self.total_it = 0
+        self.max_it = buffer_size * 24
+        
+
+    def add(self, data: dict):
+        """Adds an experience to existing memory - Oracle"""
+        if self.total_it % 24 == 0:
+            self.replay_memory.append({})
+        self.replay_memory[-1] = data
+        self.total_it += 1
+
+    def get_recent(self):
+        """Returns most recent data from memory"""
+        return (
+            self.replay_memory[-1] if len(self) > 0 and self.total_it % 24 != 0 else {}
+        )
+
+    def sample(self, is_random=False):
+        """Picks all samples within the replay_buffer"""
+        # critic 1 last n days - sequential
+        # critic 2 last n days - random
+
+        if is_random:  
+            indices = np.random.choice(
+                np.arange(len(self)), size=self.batch_size, replace=False
+            )
+
+        else:  
+            indices = np.arange(len(self) - self.batch_size, len(self))
+
+        days = [self.get(index) for index in indices]  # get all random experiences
+        # combine all days together from DataLoader
+        return days
+
+    def get(self, index):
+        """Returns an element from deque specified by `index`"""
+        try:
+            return self.replay_memory[index]
+        except IndexError:
+            print("Trying to access invalid index in replay buffer!")
+            return None
+
+    def set(self, index, data: dict):
+        """Sets an element of replay buffer w/ dictionary"""
+        try:
+            self.replay_memory[index] = data
+        except:
+            print(
+                "Trying to set replay buffer w/ either invalid index or unable to set data!"
+            )
+            return None
+
+    def __len__(self):  # override default __len__ operator
+        """Return the current size of internal memory."""
+        return len(self.replay_memory)
+
+
+    
+
 
 class reward_function_ma:
     def __init__(self, n_agents, building_info):
@@ -30,28 +101,20 @@ class reward_function_ma:
         
         # Need to include the function to get current hour
         
-        buffer_size = 24*7
-        demand_buffer = ReplayBuffer(buffer_size)
+        alpha = 5  
+        # Define ramping cost
+        ramping_cost = electricity_demand - DemandBuffer.replay_memory[-1]
         
-        if hour == 0:
-            ramping_cost = electricity_demand
+        DemandBuffer.replay_memory.append(electricty_demand)
+        
+        # Getting if at the middle of the day or the end of the day
+        hour = DemandBuffer.total_it % 24
+        
+        if hour == 23:
+            r_ = -ramping_cost - alpha*max(DemandBuffer.replay_mamory[-24:])**2
         else:
-            ramping_cost = electricity_demand - demand_buffer.get_recent()
-        
-        demand_buffer.append(electricty_demand)
-        
-        alpha = 5   
+            r_ = -ramping_cost 
             
-        # We have access to previous E_grids from the buffer
-        # Get current hour
-
-            
-        if hour <= 23:
-            r_ = -ramping_cost
-            
-        else:
-            r_ = -ramping_cost - alpha*(E_grid_pkhst)**2
-                
         reward_ = reward_.append(r_)
         
         reward_[reward_>0] = 0
