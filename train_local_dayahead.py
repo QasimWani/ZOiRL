473810b5c38a4f6425ec7533e479e59aec89ee74
Qasim Wani <qasim31wani@gmail.com>
2021-07-13 00:58:53 -0700

    adding missing parameter - bug fix

--- train_local_dayahead.py
+++ train_local_dayahead.py
@@ -70,6 +70,8 @@ done = False
 
 agents_rbc = RBC(actions_spaces)
 RBC_Egrid = []
+
+
 def get_idx_hour():
     # Finding which state
     with open("buildings_state_action_space.json") as file:
@@ -86,15 +88,15 @@ def get_idx_hour():
     return indx_hour
 
 
-
 E_grid = []
-action = agents.select_action(state,env,True)
+action = agents.select_action(state, env, True)
 state_start = state
 t_idx = 1
 numdays = 20
 end_time = RBC_THRESHOLD + 24 * numdays  # run for a month
 
 indx_hour = get_idx_hour()
+
 
 def get_rbc_data(
     surrogate_env: CityLearn, state, indx_hour: int, dump_data: list, run_timesteps: int
@@ -111,16 +113,17 @@ def get_rbc_data(
         dump_data.append([x[28] for x in state])
 
 
-
 start_time = time.time()
 cur_time = time.time()
 costs_peak_net_ele = []
 while not done and t_idx < end_time:
     print(f"\rTime step: {t_idx}", end="")
     next_state, reward, done, _ = env.step(action)
-    action_next = agents.select_action(next_state, env,True)  # passing in environment for Oracle agent.
+    action_next = agents.select_action(
+        next_state, env, True
+    )  # passing in environment for Oracle agent.
 
-    agents.add_to_buffer_oracle(state, env, action, reward)
+    agents.add_to_buffer_oracle(state, env, action, reward, next_state)
     # agents.add_to_buffer(state, action, reward, next_state, done)
     state = next_state
     action = np.array(action_next)
@@ -176,7 +179,6 @@ vars_RL = agents.logger
 # vars_NORL = agents.norl_loggerd
 
 
-
 debug_item = [
     "E_grid",
     "E_bal_relax",
@@ -202,7 +204,14 @@ check_data_param = {}
 for key in debug_item:
     check_data[key] = [[] for i in range(9)]
 
-data_param_keys = ["c_bat_init", "c_Csto_init", "c_Hsto_init", "C_p_bat", "C_p_Csto", "C_p_Hsto"]
+data_param_keys = [
+    "c_bat_init",
+    "c_Csto_init",
+    "c_Hsto_init",
+    "C_p_bat",
+    "C_p_Csto",
+    "C_p_Hsto",
+]
 for key in data_param_keys:
     if key in ["C_p_bat", "C_p_Csto", "C_p_Hsto"]:
         check_data_param[key] = [[] for i in range(9)]
@@ -228,7 +237,7 @@ for key in data_param_keys:
         for bid in range(9):
             optim_var = agents.logger_data[i]
             if key in ["C_p_bat", "C_p_Csto", "C_p_Hsto"]:
-                check_data_param[key][bid].extend(np.array(optim_var[key][:,bid]))
+                check_data_param[key][bid].extend(np.array(optim_var[key][:, bid]))
             else:
                 check_data_param[key][bid].append((optim_var[key][bid]))
     check_data_param[key][bid] = np.array(check_data_param[key][bid])
@@ -239,7 +248,7 @@ for bid in range(9):
     E_grid_pred[bid] = check_data["E_grid"][bid]
     E_grid_sell_pred[bid] = check_data["E_grid_sell"][bid]
 # plot predicted E_grid
-sthour = 24*5  # plots last week of the month data
+sthour = 24 * 5  # plots last week of the month data
 fig, axs = plt.subplots(3, 3, figsize=(15, 15))
 for i in range(3):
     for j in range(3):
@@ -285,7 +294,7 @@ for key in debug_item:
 
 env_comp_item = ["electric_consumption_cooling", "electric_consumption_dhw"]
 env_comp_item_check = ["E_hpC", "E_ehH"]
-sthour = 24*7
+sthour = 24 * 7
 for key_i in range(len(env_comp_item)):
     data_np = np.array(check_data[env_comp_item_check[key_i]])
 
@@ -320,7 +329,7 @@ env_comp_item_check2 = ["SOC_bat", "SOC_C", "SOC_H"]
 env_comp_item_check3 = ["C_p_bat", "C_p_Csto", "C_p_Hsto"]
 env_comp_item_check4 = ["c_bat_init", "c_Csto_init", "c_Hsto_init"]
 
-sthour = 24*plot_days
+sthour = 24 * plot_days
 week = end_time - 24 * plot_days
 
 for key_i in range(len(env_comp_item)):
@@ -331,28 +340,59 @@ for key_i in range(len(env_comp_item)):
         for j in range(3):
             bid = i * 3 + j
             data_env = np.array(
-                getattr(getattr(env.buildings["Building_" + str(bid + 1)], env_comp_item[key_i]
-                    ), "energy_balance"))
+                getattr(
+                    getattr(
+                        env.buildings["Building_" + str(bid + 1)], env_comp_item[key_i]
+                    ),
+                    "energy_balance",
+                )
+            )
             data_env2 = np.array(
-                getattr(getattr(env.buildings["Building_" + str(bid + 1)], env_comp_item[key_i]
-                    ),"soc"))
+                getattr(
+                    getattr(
+                        env.buildings["Building_" + str(bid + 1)], env_comp_item[key_i]
+                    ),
+                    "soc",
+                )
+            )
             axs[i, j].set_title(f"Building {bid + 1}: {env_comp_item[key_i]}")
             axs[i, j].plot(
-                data_np[bid][-sthour:] * check_data_param[env_comp_item_check3[key_i]][bid][0],
+                data_np[bid][-sthour:]
+                * check_data_param[env_comp_item_check3[key_i]][bid][0],
                 label="optimization",
             )  # plot true E grid
             axs[i, j].plot(
-                data_np2[bid][-sthour:] * check_data_param[env_comp_item_check3[key_i]][bid][0],
+                data_np2[bid][-sthour:]
+                * check_data_param[env_comp_item_check3[key_i]][bid][0],
                 label="optimization SOC",
             )
-            axs[i, j].scatter(np.arange(-1,plot_days*24-1,24),np.array(check_data_param[env_comp_item_check4[key_i]][bid][-plot_days:])* float(check_data_param[env_comp_item_check3[key_i]][bid][0]),s=50,marker='x',
+            axs[i, j].scatter(
+                np.arange(-1, plot_days * 24 - 1, 24),
+                np.array(
+                    check_data_param[env_comp_item_check4[key_i]][bid][-plot_days:]
+                )
+                * float(check_data_param[env_comp_item_check3[key_i]][bid][0]),
+                s=50,
+                marker="x",
                 label="optimization init SOC",
             )
-            axs[i, j].scatter(np.arange(0, plot_days * 24, 24),
-                              (np.array(check_data_param[env_comp_item_check4[key_i]][bid][-plot_days:])+data_np[bid][np.array(range(week-RBC_THRESHOLD,end_time-RBC_THRESHOLD,24))]) * float(
-                                  check_data_param[env_comp_item_check3[key_i]][bid][0]), s=50,marker='x',
-                              label="optimization SOC t=1",
-                              )
+            axs[i, j].scatter(
+                np.arange(0, plot_days * 24, 24),
+                (
+                    np.array(
+                        check_data_param[env_comp_item_check4[key_i]][bid][-plot_days:]
+                    )
+                    + data_np[bid][
+                        np.array(
+                            range(week - RBC_THRESHOLD, end_time - RBC_THRESHOLD, 24)
+                        )
+                    ]
+                )
+                * float(check_data_param[env_comp_item_check3[key_i]][bid][0]),
+                s=50,
+                marker="x",
+                label="optimization SOC t=1",
+            )
             axs[i, j].plot(data_env[-sthour:], label="environment")  # plot true E grid
             axs[i, j].plot(
                 data_env2[-sthour:], label="environment SOC"
@@ -377,14 +417,26 @@ for key_i in range(len(env_comp_item)):
         for j in range(3):
             bid = i * 3 + j
             data_env = np.array(
-                getattr(getattr(env.buildings["Building_" + str(bid + 1)], env_comp_item[key_i]
-                    ), "energy_balance"))
+                getattr(
+                    getattr(
+                        env.buildings["Building_" + str(bid + 1)], env_comp_item[key_i]
+                    ),
+                    "energy_balance",
+                )
+            )
             data_env2 = np.array(
-                getattr(getattr(env.buildings["Building_" + str(bid + 1)], env_comp_item[key_i]
-                    ),"soc"))
+                getattr(
+                    getattr(
+                        env.buildings["Building_" + str(bid + 1)], env_comp_item[key_i]
+                    ),
+                    "soc",
+                )
+            )
             axs[i, j].set_title(f"Building {bid + 1}: {env_comp_item[key_i]}")
             axs[i, j].plot(
-                data_np2[bid][-sthour:] * check_data_param[env_comp_item_check3[key_i]][bid][0]-data_env2[-sthour:],
+                data_np2[bid][-sthour:]
+                * check_data_param[env_comp_item_check3[key_i]][bid][0]
+                - data_env2[-sthour:],
                 label="optim-environ SOC",
             )
 
@@ -395,7 +447,8 @@ for key_i in range(len(env_comp_item)):
                 axs[i, j].set_xlabel("Hour")
     plt.legend()
     fig.savefig(
-        f"images/{env_comp_item_check[key_i]}_optim-diff-env_plot.pdf", bbox_inches="tight"
+        f"images/{env_comp_item_check[key_i]}_optim-diff-env_plot.pdf",
+        bbox_inches="tight",
     )
 
 # Get actions
@@ -421,7 +474,7 @@ for key_i in range(len(env_comp_item)):
 # Compare the ramping, peak electricity costs
 eval_days = 20
 week = end_time - 24 * eval_days  # plots last week of the month data
-sthour = 24*eval_days
+sthour = 24 * eval_days
 ramping_cost_optim = []
 ramping_cost_RBC = []
 peak_electricity_cost_optim = []
@@ -496,8 +549,10 @@ fig.savefig(f"images/total_cost_ratio.pdf", bbox_inches="tight")
 
 fig1, ax1 = plt.subplots()
 ax1.set_title(f"Total cost community Optim/RBC")
-ax1.plot(np.sum(Optim_cost["total_cost"],axis=0) / np.sum(RBC_cost["total_cost"],axis=0),
-            label=f"Optim/RBC")  # plot true E grid
+ax1.plot(
+    np.sum(Optim_cost["total_cost"], axis=0) / np.sum(RBC_cost["total_cost"], axis=0),
+    label=f"Optim/RBC",
+)  # plot true E grid
 ax1.grid()
 ax1.set_ylabel("Cost (Ratio)")
 ax1.set_xlabel("Day")
