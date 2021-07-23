3b28f4c364c29fb67fa47244dddd5df238416d16
Qasim Wani <qasim31wani@gmail.com>
2021-07-22 21:37:29 -0700

    updating digital twin API

--- digital_twin.py
+++ digital_twin.py
@@ -3,7 +3,7 @@ class DigitalTwin:
     def __init__(self) -> None:
         pass
 
-    def transition(self):
+    def transition(self, current_state, actions, total_it, memory):
         raise NotImplementedError
 
     def step(self):
