e8bf23863c87993c0a9e4d53960daecbcd4fafb1
Qasim Wani <qasim31wani@gmail.com>
2022-01-03 04:49:12 +0300

    adding logger

---
+++ logger.py
@@ -0,0 +1,16 @@
+from IPython.display import clear_output
+import os
+
+
+clear = lambda: os.system("cls")  # for terminal
+
+### Prints details and error logs.
+def LOG(data: str, pprint: bool = True, file: str = "log.txt"):
+    f = open(file, "a")
+    f.write(data + "\n")
+    f.close()
+
+    if pprint:
+        print("\r" + data, end="")
+        clear_output(wait=True)
+        clear()
